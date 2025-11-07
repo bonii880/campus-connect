65af051f7f005121be1f46e15d70d6078882b161
bonii880 <b32452834@gmail.com>
2025-11-07 16:08:17 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,19 @@
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-auth.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyARcZM2zSy4Xw5Q7Fn_XNX9zDh2LMAQvqw",
+  authDomain: "campusconnect-44ec3.firebaseapp.com",
+  projectId: "campusconnect-44ec3",
+  storageBucket: "campusconnect-44ec3.appspot.com",
+  messagingSenderId: "440410311105",
+  appId: "1:440410311105:web:0bb84ca67f0407235d4740",
+  measurementId: "G-JZ8R9VS9QC"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth };
